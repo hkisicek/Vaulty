52d21c72c932b2d0848706e742be7719e0cb767c
hkisicek <hkisicek@foi.hr>
2017-08-16 13:47:09 +0200

    Finally functioning redirect function

---
+++ Core/Redirect.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: helena
+ * Date: 8/16/17
+ * Time: 1:36 PM
+ */
+
+class Redirect{
+
+    /**
+     * @param $url
+     */
+    public static function redirectUrl($url)
+    {
+        if (!headers_sent())
+        {
+            header('Location: '.$url);
+            exit;
+        }
+        else
+        {
+            echo '<script type="text/javascript">';
+            echo 'window.location.href="'.$url.'";';
+            echo '</script>';
+            echo '<noscript>';
+            echo '<meta http-equiv="refresh" content="0;url='.$url.'" />';
+            echo '</noscript>'; exit;
+        }
+    }
+
+}
